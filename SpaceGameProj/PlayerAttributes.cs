0121f6a5dd8b2368158abb65547b4585fb7262d6
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-08-09 09:01:20 -0400

    Updating Files

---
+++ SpaceGameProj/PlayerAttributes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGameProj
+{
+    public class Attributes
+    {
+
+
+        public int BonusCargo { get; set; }
+        public int BonusFuel { get; set; }
+        public int BonusMoney { get; set; }
+
+        public Attributes(int bonusFuel, int bonusMoney, int bonusCargo)
+        {
+            this.BonusCargo = bonusCargo * 10;
+            this.BonusFuel = bonusFuel * 10;
+            this.BonusMoney = bonusMoney * 50;
+
+        }
+    }
+}
