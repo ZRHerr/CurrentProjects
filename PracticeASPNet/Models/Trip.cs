91865fbc51c20173bf5d9d80b6e07b52d7fac696
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-08-11 15:06:08 -0400

    //Classes built so i can test build the API.

---
+++ PracticeASPNet/Models/Trip.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticeASPNet.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
