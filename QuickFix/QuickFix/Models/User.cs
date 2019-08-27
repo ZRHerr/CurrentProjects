c3876e2ecd998481a96470297947afbc7d835193
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-08-27 12:11:42 -0400

    Final Project restart using MVC// Finished Registration page and connection to database, TODO design layout, create the rest of the pages to connect.

---
+++ QuickFix/QuickFix/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace QuickFix.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(5), Column(TypeName = "nvarchar")]
+        public string Username { get; set; }
+
+        [MinLength(5), Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [EmailAddress, Index(IsUnique = true), Column(TypeName = "nvarchar"), Required]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string ImageUrl { get; set; }
+
+        [ScaffoldColumn(false)]
+        public DateTime? CreatedOn { get; set; }
+
+
+
+
+
+    }
+}
