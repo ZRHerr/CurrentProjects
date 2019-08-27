55a6a96dd9dd702d1645dd3a90041251b96adbe5
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-08-27 12:11:42 -0400

    Final Project restart using MVC// Finished Registration page and connection to database, TODO design layout, create the rest of the pages to connect.

---
+++ QuickFix/QuickFix/Models/Reply.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace QuickFix.Models
+{
+    public class Reply
+    {
+        [Key]
+        public int ReplyId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public int MessageId { get; set; }
+
+        [ForeignKey("MessageId")]
+        public virtual Message Message { get; set; }
+
+
+
+    }
+}
---
+++ QuickFix/QuickFix/ViewModels/Registration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace QuickFix.ViewModels
+{
+    public class Registration
+    {
+        //This data doesnt need to be compared with the data within the database
+        //The passwords should be compared to ensure password creation is what the user intended
+        [Required, MinLength(5)]
+        public string Username { get; set; }
+
+        [MinLength(5), Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password"),DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [EmailAddress, Required]
+        public string Email { get; set; }
+
+    }
+}
