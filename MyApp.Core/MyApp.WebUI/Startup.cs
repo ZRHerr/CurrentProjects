22cf834d4095f47b53bc661a8ffb46ebdbd3859c
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-09-01 16:53:09 -0400

    Initial Project Structure

---
+++ MyApp.Core/MyApp.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyApp.WebUI.Startup))]
+namespace MyApp.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
