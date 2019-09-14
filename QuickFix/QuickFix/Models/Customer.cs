003702872229086f7003c39a0932435c99f50103
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-09-14 13:06:23 -0400

    Started Final Project, Finished base models and a mock repository for testing purposes.

---
+++ QuickFix/QuickFix/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickFix.Models
+{
+    public class Customer
+    {
+        public int CustId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public decimal ContactNumber { get; set; }
+        public string Email { get; set; }
+        public byte[] Image { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+}
---
+++ QuickFix/QuickFix/Models/IServiceRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickFix.Models
+{
+    public interface IServiceRepository
+    {
+        IEnumerable<Services> GetServices();
+
+        Services GetServiceById(int serviceId);
+    }
+}
