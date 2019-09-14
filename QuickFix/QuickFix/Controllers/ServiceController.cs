1d8a254feaf925e57bff87fa26a23488bcb43018
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-09-14 13:32:28 -0400

    Added ViewModel for services to clean up view. Added Functionality to new ViewModel in serviceController.

---
+++ QuickFix/QuickFix/Controllers/ServiceController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuickFix.Models;
+using QuickFix.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuickFix.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IServiceRepository _serviceReposity;
+
+        public ServiceController(IServiceRepository serviceRepository)
+        {
+            _serviceReposity = serviceRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var services = _serviceReposity.GetServices().OrderBy(s => s.ServiceType);
+            var serviceViewModel = new ServiceViewModel()
+            {
+                Title = "All active and past services",
+                services = services.ToList()
+            };
+            return View(serviceViewModel);
+        }
+    }
+}
---
+++ QuickFix/QuickFix/ViewModels/ServiceViewModel.cs
@@ -0,0 +1,14 @@
+﻿using QuickFix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickFix.ViewModels
+{
+    public class ServiceViewModel
+    {
+        public string Title { get; set; }
+        public List<Services> services { get; set; }
+    }
+}
