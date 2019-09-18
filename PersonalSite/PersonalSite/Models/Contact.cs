cf34794b7a5d9773791c81814b0ea1ea47150099
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-09-17 23:24:47 -0400

    Put a ton of work in tonight - Almost a finished project feel free to check it out and shoot me some feedback. Still need to add links to my social media and my current projects.

---
+++ PersonalSite/PersonalSite/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalSite.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int MessageId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Messege { get; set; }
+    }
+}
---
+++ PersonalSite/PersonalSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PersonalSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
