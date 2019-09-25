6f11d7eedcf2e51e4b143a638885047391b935a6
Zachary Herrera <53063781+ZRHerr@users.noreply.github.com>
2019-09-25 13:12:56 -0400

    Simplified Version of Encrypting locally using a console application.

---
+++ PasswordEncryption/PasswordEncryption/Program.cs
@@ -0,0 +1,104 @@
+﻿using PasswordEncryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordEncryption
+{
+    class Program
+    {
+        //1.	save a new password for a specific username,
+        //2.	authenticate a specific username/password pair, or
+        //3.	exit the application.
+        //4.	On exit, all username/password pairs will be lost
+              static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            int choice;
+            do
+            {
+
+                Console.WriteLine("1. Establish an account");
+                Console.WriteLine("2. Authenticate a user");
+                Console.WriteLine("3. Exit the system");
+                Console.WriteLine();
+                Console.Write("Enter selection: ");
+                string option = Console.ReadLine();
+                choice = Int32.Parse(option);
+                string username;
+                string password;
+                switch (choice)
+                {
+                    case 1:
+                        
+                        Console.Write("Enter Username: ");
+                        username = Console.ReadLine();
+                        Console.Write("Enter Password: ");
+                        password = Console.ReadLine();
+                        bool unique = true;
+                        if(users.Count>0)
+                        {
+                            for (int i = 0; i < users.Count; i++)
+                            {
+                                if (users[i].UserExist(username))
+                                {
+                                    Console.WriteLine("Username Already exist. Try Again");
+                                    unique = false;
+                                }
+                            }
+                            if (!unique)
+                            {
+                                User newuser = new User(username, password);
+                                users.Add(newuser);
+                            }
+                        }
+                        else
+                        {
+                            User newuser = new User(username, password);
+                            users.Add(newuser);
+                        }
+                        break;
+                    case 2:
+                        Console.Write("Enter Username: ");
+                        username = Console.ReadLine();
+                        Console.Write("Enter Password: ");
+                        password = Console.ReadLine();
+                        bool userexist = false;
+                        for (int i = 0; i < users.Count; i++)
+                        {
+                            if (users[i].UserExist(username))
+                            {
+                                userexist = true;
+                                if (users[i].Login(password))
+                                {
+                                    Console.WriteLine("Authenticated");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Not Authenticated");
+                                    break;
+                                }
+
+                            }
+                        }
+                        if (!userexist)
+                        {
+                            Console.WriteLine("Username Does not exist");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("good bye!");
+                        break;
+                    default:
+                        Console.WriteLine("enter valid option");
+                        break;
+                  }
+                Console.WriteLine();
+            } while (choice != 3);
+
+        }
+    }
+}
---
+++ PasswordEncryption/PasswordEncryption/User.cs
@@ -0,0 +1,182 @@
+﻿class User
+{
+    readonly string cipher;
+    readonly string username;
+    public User(string name, string pass)
+    {
+        username = name;
+        cipher = Encrypt(pass);
+    }
+    //take the password user enter for user and ecrypt and return that will be stored in our cipher string.
+    public string Encrypt(string password)
+    {
+        password = password.ToLower();
+        char[] ch = password.ToCharArray();
+        string encrypted = "";
+        for (int i = 0; i < password.Length; i++)
+        {
+            switch (ch[i])
+            {
+                case 'a':
+                    encrypted +=  "{";
+                    break;
+                case 'b':
+                    encrypted +=  "}";
+                    break;
+                case 'c':
+                    encrypted +=  "#";
+                    break;
+                case 'd':
+                    encrypted += "~";
+                    break;
+                case 'e':
+                    encrypted += "+";
+                    break;
+                case 'f':
+                    encrypted +=  "-";
+                    break;
+                case 'g':
+                    encrypted += "*";
+                    break;
+                case 'h':
+                    encrypted +=  "@";
+                    break;
+                case 'i':
+                    encrypted +=  "/";
+                    break;
+                case 'j':
+                    encrypted +=  "\\";
+                    break;
+                case 'k':
+                    encrypted += "?";
+                    break;
+                case 'l':
+                    encrypted +=  "$";
+                    break;
+                case 'm':
+                    encrypted += "!";
+                    break;
+                case 'n':
+                    encrypted += "^";
+                    break;
+                case 'o':
+                    encrypted +=  "(";
+                    break;
+                case 'p':
+                    encrypted +=  ")";
+                    break;
+                case 'q':
+                    encrypted +=  "<";
+                    break;
+                case 'r':
+                    encrypted += ">";
+                    break;
+                case 's':
+                    encrypted +=  "=";
+                    break;
+                case 't':
+                    encrypted +=  ";";
+                    break;
+                case 'u':
+                    encrypted +=  ",";
+                    break;
+                case 'v':
+                    encrypted +=  "_";
+                    break;
+                case 'w':
+                    encrypted += "[";
+                    break;
+                case 'x':
+                    encrypted += "]";
+                    break;
+                case 'y':
+                    encrypted +=  ":";
+                    break;
+                case 'z':
+                    encrypted +=  "\"";
+                    break;
+                case ' ':
+                    encrypted += " ";
+                    break;
+                case '.':
+                    encrypted += '3';
+                    break;
+                case ',':
+                    encrypted +=  "1";
+                    break;
+                case '(':
+                    encrypted += '4';
+                    break;
+                case '\"':
+                    encrypted += '5';
+                    break;
+                case ')':
+                    encrypted +=  "7";
+                    break;
+                case '?':
+                    encrypted +=  "2";
+                    break;
+                case '!':
+                    encrypted +=  "8";
+                    break;
+                case '-':
+                    encrypted +=  "6";
+                    break;
+                case '%':
+                    encrypted +=  "9";
+                    break;
+                case '1':
+                    encrypted +=  "r";
+                    break;
+                case '2':
+                    encrypted +=  "k";
+                    break;
+                case '3':
+                    encrypted +=  "b";
+                    break;
+                case '4':
+                    encrypted +=  "e";
+                    break;
+                case '5':
+                    encrypted +=  "q";
+                    break;
+                case '6':
+                    encrypted +=  "h";
+                    break;
+                case '7':
+                    encrypted +=  "u";
+                    break;
+                case '8':
+                    encrypted += "y";
+                    break;
+                case '9':
+                    encrypted +=  "w";
+                    break;
+                case '0':
+                    encrypted +=  "z";
+                    break;
+                default:
+                    encrypted +=  "0";
+                    break;
+            }
+        }
+        return encrypted;
+    }
+    //check if username already exist or not
+   public bool UserExist(string user)
+    {
+        if (username.Equals(user))
+        {
+            return true;
+        }
+        return false;
+    }
+    //return true if passwords match, it actually match both encrypted passwords,
+   public bool Login(string pass)
+    {
+        string password = Encrypt(pass);
+        if (cipher.Equals(password))
+            return true;
+        return false;
+    }
+}
